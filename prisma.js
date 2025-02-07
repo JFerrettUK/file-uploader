5548f31cd73c89f180bfe549839e1fd7a1aae423
JFerrettUK <jferrettuk@gmail.com>
2025-02-07 08:54:47 +0000

    prisma update

--- prisma.js
+++ prisma.js
@@ -1,5 +1,12 @@
+// prisma/index.js (or your Prisma client initialization file)
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL, // Use the Koyeb-provided environment variable
+    },
+  },
+});
 
 module.exports = prisma;
